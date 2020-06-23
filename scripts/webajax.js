cda644d567dfd9cabea7793994f30982cefaee9e
Eze Heredia <64047925+mherediacifuentes@users.noreply.github.com>
2020-06-23 20:20:24 -0300

    Ajax
    Update Ajax Script

---
+++ scripts/webajax.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $("#main").load("home.html");
+    $("#home").click(function(){
+        $("#main").load("home.html");
+    });
+    $("#faqs").click(function(){
+        $("#main").load("faqs.html");
+    });
+    $("#producto").click(function(){
+        $("#main").load("producto.html");
+    });
+    $("#producto").click(function(){
+        $("#main").load("producto.html");
+    });
+    $("#termycond").click(function(){
+        $("#main").load("terminosycondiciones.html");
+    });
+});
